427ec89f862116e89ecefff06dd7dcd7ccb39656
PRINCE123853 <princekumar2005itaha@gmail.com>
2026-06-14 09:07:53 +0530

    add base url for deployment

--- MERN_Project_03_blog-app/frontend/src/utils/api.js
+++ MERN_Project_03_blog-app/frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://blog-app-backend-6c0y.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 10000,
 });
